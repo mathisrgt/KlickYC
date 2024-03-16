730447b7ffd6d2f6d797f6c8a8e143c779ce7c01
mathis sergent <sergent.mathis@gmail.com>
2024-03-16 15:29:20 +0000

    front-end
    front-end

---
+++ front/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "KlickYC",
+  description: "KlickYC is a KYC oracle that allows you to identify a user in just a few clicks using open-banking.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
